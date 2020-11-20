ca3f20bab483477a0014ad3962d152df7eaa0e3d
amin nikookaran <a.nikookaran@sls.ir>
2020-11-21 01:36:24 +0330

    .

---
+++ src/main/java/problems/linkedlists/GetNodeValue.java
@@ -0,0 +1,33 @@
+package problems.linkedlists;
+
+public class GetNodeValue {
+  public static class SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int data) {
+      this.data = data;
+    }
+  }
+
+  public static int getNode(SinglyLinkedListNode head, int positionFromTail) {
+    SinglyLinkedListNode temp = head;
+    int size = 0;
+    while (temp != null) {
+      temp = temp.next;
+      size++;
+    }
+    if (positionFromTail >= size) return -1;
+    int positionFromHead = size - positionFromTail - 1;
+    temp = head;
+    while (positionFromHead > 0) {
+      temp = temp.next;
+      positionFromHead--;
+    }
+    return temp.data;
+  }
+
+  public static void main(String[] args) {
+    //    System.out.println(reverse());
+  }
+}
---
+++ src/main/java/problems/linkedlists/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,75 @@
+package problems.linkedlists;
+
+public class MergeTwoSortedLinkedLists {
+  public static class SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int data) {
+      this.data = data;
+    }
+  }
+
+  public static SinglyLinkedListNode mergeLists(
+      SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+    if (head1 == null) return head2;
+    if (head2 == null) return head1;
+    if (head2.data < head1.data) {
+      SinglyLinkedListNode temp = head1;
+      head1 = head2;
+      head2 = temp;
+    }
+    SinglyLinkedListNode head = head1;
+    SinglyLinkedListNode prehead1 = head1;
+    head1 = head1.next;
+    while (head1 != null && head2 != null) {
+      if (head1.data <= head2.data) {
+        head1 = head1.next;
+      } else {
+        prehead1.next = head2;
+        head2 = head2.next;
+        prehead1.next.next = head1;
+      }
+      prehead1 = prehead1.next;
+    }
+    if (head2 != null) prehead1.next = head2;
+
+    return head;
+  }
+
+  public static SinglyLinkedListNode sortedMerge(
+      SinglyLinkedListNode headA, SinglyLinkedListNode headB) {
+    SinglyLinkedListNode head = new SinglyLinkedListNode(0);
+    SinglyLinkedListNode tail = head;
+    while (headA != null && headB != null) {
+      if (headA.data < headB.data) {
+        tail.next = headA;
+        headA = headA.next;
+      } else {
+        tail.next = headB;
+        headB = headB.next;
+      }
+      tail = tail.next;
+    }
+    if (headA != null) tail.next = headA;
+    if (headB != null) tail.next = headB;
+    return head.next;
+  }
+
+  public SinglyLinkedListNode SortedMerge(SinglyLinkedListNode A, SinglyLinkedListNode B) {
+    if (A == null) return B;
+    if (B == null) return A;
+
+    if (A.data < B.data) {
+      A.next = SortedMerge(A.next, B);
+      return A;
+    } else {
+      B.next = SortedMerge(A, B.next);
+      return B;
+    }
+  }
+
+  public static void main(String[] args) {
+    //    System.out.println(reverse());
+  }
+}
